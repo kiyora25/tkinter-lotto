117ba637aecd5190074d2b6f9df84d519fded573
kiyora <kiyorapis@gmail.com>
2023-04-27 15:41:53 +0900

    lotto main.py fourth commit

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import random
 
 lotto_num = range(1,46)
 
-def buttonClick():
-    print(random.sample(lotto_num,6))
+def  buttonClick():
+    label = tkinter.Label(window, text=str(random.sample(lotto_num,6)))
+    label.pack()
 
 window=tkinter.Tk()
 window.title("lotto")
